8100779ce41d71ceafea95a894a997fde7a4f552
James Wilson <me@unbui.lt>
2018-11-04 17:00:35 +0000

    a couple of optimisations

--- src/common/platter.rs
+++ src/common/platter.rs
@@ -20,23 +20,26 @@ impl Platter {
     pub fn to_operator(self) -> Option<Operator> {
         let val = self.0;
         let op_num = (val >> 28) & 15;
-        let (a,b,c) = self.extract_standard_op_registers();
+
+        let a = || ((val >> 6) & 7) as usize;
+        let b = || ((val >> 3) & 7) as usize;
+        let c = || (val & 7) as usize;
 
         use self::Operator::*;
         match op_num {
-            0 => Some(ConditionalMove{a,b,c}),
-            1 => Some(ArrayIndex{a,b,c}),
-            2 => Some(ArrayAmendment{a,b,c}),
-            3 => Some(Addition{a,b,c}),
-            4 => Some(Multiplication{a,b,c}),
-            5 => Some(Division{a,b,c}),
-            6 => Some(NotAnd{a,b,c}),
+            0 => Some(ConditionalMove{a:a(), b:b(), c:c()}),
+            1 => Some(ArrayIndex{a:a(), b:b(), c:c()}),
+            2 => Some(ArrayAmendment{a:a(), b:b(), c:c()}),
+            3 => Some(Addition{a:a(), b:b(), c:c()}),
+            4 => Some(Multiplication{a:a(), b:b(), c:c()}),
+            5 => Some(Division{a:a(), b:b(), c:c()}),
+            6 => Some(NotAnd{a:a(), b:b(), c:c()}),
             7 => Some(Halt),
-            8 => Some(Allocation{b,c}),
-            9 => Some(Abandonment{c}),
-            10 => Some(Output{c}),
-            11 => Some(Input{c}),
-            12 => Some(LoadProgram{b,c}),
+            8 => Some(Allocation{b:b(), c:c()}),
+            9 => Some(Abandonment{c:c()}),
+            10 => Some(Output{c:c()}),
+            11 => Some(Input{c:c()}),
+            12 => Some(LoadProgram{b:b(), c:c()}),
             13 => {
                 let a = (val >> 25) & 7;
                 let value = val & 0b0000_000_1111111111111111111111111;
@@ -44,13 +47,6 @@ impl Platter {
             },
             _ => None
         }
-    }
-    pub fn extract_standard_op_registers(self) -> (usize, usize, usize) {
-        let val = self.0;
-        let c = val & 7;
-        let b = (val >> 3) & 7;
-        let a = (val >> 6) & 7;
-        (a as usize, b as usize, c as usize)
     }
 }
 
